e665e6a74198d30e0fd93bc77ed404bef98b4a2a
nguyenduckhiem1002-dot <nguyenduckhiem1002@gmail.com>
2026-09-20 22:34:08 +0700

    feat: Việt hóa và đổi thương hiệu Casla Assets

--- apps/webapp/app/routes/_layout+/kits.tsx
+++ apps/webapp/app/routes/_layout+/kits.tsx
@@ -2,14 +2,14 @@ import { Link, Outlet } from "react-router";
 import { ErrorContent } from "~/components/errors";
 import { appendToMetaTitle } from "~/utils/append-to-meta-title";
 
-export const meta = () => [{ title: appendToMetaTitle("Kits") }];
+export const meta = () => [{ title: appendToMetaTitle("Bộ tài sản") }];
 
 export function loader() {
   return null;
 }
 
 export const handle = {
-  breadcrumb: () => <Link to="/kits">Kits</Link>,
+  breadcrumb: () => <Link to="/kits">Bộ tài sản</Link>,
 };
 
 export default function Kits() {
